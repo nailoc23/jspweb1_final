a3be4be3c83e441fc255b24f5169bf83eb551207
nailoc23 <nailoc23@naver.com>
2024-04-26 12:53:32 +0900

    MemberDao 수정 - int selectEmpByIdPw(String id, String pw)

--- jspweb1_final/src/main/java/com/touzone/stsb/dao/MemberDao.java
+++ jspweb1_final/src/main/java/com/touzone/stsb/dao/MemberDao.java
@@ -1,5 +1,7 @@
 package com.touzone.stsb.dao;
 
+import java.sql.SQLException;
+
 public class MemberDao extends DBManager {
 	
 	/**
@@ -11,6 +13,35 @@ public class MemberDao extends DBManager {
 			instance=new MemberDao();
 		}
 		return instance;
+	}
+	
+	/**
+	 * 디비를 접속해서 id pw 가 존재하는지 체크하는 함수
+	 * @param id
+	 * @param pw
+	 * @return
+	 */
+	public int selectEmpByIdPw(String id, String pw) {
+		
+		int result=0;
+		conn = getConnect();
+		try {
+			String sql = "SELECT count(*) as cnt FROM members WHERE memid=? and password=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,id);
+			pstmt.setString(2,pw);
+			// rs : resultset
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				result = rs.getInt("cnt");
+			}
+		}catch(SQLException se) {
+			System.out.println("selectEmpByIdPw Error:");
+		}finally {
+			DBClose();
+		}
+		 
+		return result;
 	}
 
 }
