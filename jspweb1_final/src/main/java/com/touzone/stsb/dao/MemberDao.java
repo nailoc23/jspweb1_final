681ca551b5cbea6c0cb755e9f713afefafd4be34
nailoc23 <nailoc23@naver.com>
2024-05-03 15:50:36 +0900

    회원정보 가져오기 및 업데이트 수정

--- jspweb1_final/src/main/java/com/touzone/stsb/dao/MemberDao.java
+++ jspweb1_final/src/main/java/com/touzone/stsb/dao/MemberDao.java
@@ -101,5 +101,64 @@ public class MemberDao extends DBManager {
 		
 		return result;
 	}
+	
+	/**
+	 * 디비를 접속해서 id pw 가 존재하는지 체크하는 함수
+	 * @param id
+	 * @param pw
+	 * @return
+	 */
+	public MemberVo selectMemberOneById(String id) {
+		
+		MemberVo result= new MemberVo();
+		conn = getConnect();
+		try {
+			String sql = "SELECT * FROM members WHERE memid=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,id);			
+			// rs : resultset
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				result.setName( rs.getString("name") );
+				result.setEmail(rs.getString("email"));
+				result.setPhone(rs.getString("phone"));
+			}
+		}catch(SQLException se) {
+			System.out.println("selectMemberOneById Error:" + se.getMessage() );
+		}finally {
+			DBClose();
+		}
+		 
+		return result;
+	}
+	
+	public int updateMemberById(MemberVo membervo) {
 
+		int result=0; // 수정결과 1이상: 성공
+		
+		conn = getConnect();
+		
+		try {
+			StringBuffer sb = new StringBuffer();
+			sb.append("UPDATE members SET name=? , \n");
+			sb.append("               email = ? , \n");
+			sb.append("               phone = ? , \n");
+			sb.append(" WHERE memid=?" );
+			String sql = sb.toString();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,  membervo.getName() );
+			pstmt.setString(2,  membervo.getEmail() );
+			pstmt.setString(3,  membervo.getPhone() );
+			pstmt.setString(4,  membervo.getMemid() );
+			result = pstmt.executeUpdate();
+		}catch(SQLException se) {
+			System.out.println("updateMemberById Error:" + se.getMessage());
+		}finally {
+			DBClose();
+		}
+		
+		return result;
+	}
 }
+
+
