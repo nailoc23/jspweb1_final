b62816704dc5a3be5dcff03d3de719b2a7b5d7f1
nailoc23 <nailoc23@naver.com>
2024-05-07 17:53:19 +0900

    게시판 Dao 목록 가져오기 수정

--- jspweb1_final/src/main/java/com/touzone/stsb/dao/BoardDao.java
+++ jspweb1_final/src/main/java/com/touzone/stsb/dao/BoardDao.java
@@ -1,5 +1,11 @@
 package com.touzone.stsb.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.touzone.stsb.vo.BoardVo;
+
 public class BoardDao extends DBManager {
 	
 	/**
@@ -11,6 +17,39 @@ public class BoardDao extends DBManager {
 			instance=new BoardDao();
 		}
 		return instance;
+	}
+	
+	/**
+	 * 게시판 목록들을 가져오기
+	 * @return
+	 */
+	public List<BoardVo> selectBoardList() {
+		
+		List<BoardVo> list = new ArrayList<BoardVo>();
+		
+		conn=getConnect();
+		
+		try {
+			stmt = conn.createStatement();
+			String sql = "SELECT num,subject, name, DATE_FORMAT(regdate, '%Y-%m-%d') as regdate, hit FROM BOARDS ORDER BY num DESC";
+			// rs : ResultSet 
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				BoardVo tempVo = new BoardVo();
+				tempVo.setNum(rs.getInt("num"));
+				tempVo.setSubject(rs.getString("subject"));
+				tempVo.setName(rs.getString("name"));
+				tempVo.setRegdate(rs.getString("regdate"));
+				tempVo.setHit(rs.getInt("hit"));
+				list.add(tempVo);
+			}
+		}catch(SQLException se) {
+			System.out.println("selectBoardList error:" + se.getMessage() );
+		}finally {
+			DBClose();
+		}
+				
+		return list;
 	}
 
 }
